63774373a8f4b9de2d10cbf25bd35bd3a622eae6
ahmedmatlaya25 <ahmed.matlaya@gmail.com>
2020-08-28 13:55:42 +0100

    first msg

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var red = onclick="document.getElementById('body').style.backgroundColor=' darkred'"
+var blue = onclick="document.getElementById('body').style.backgroundColor=' darkblue'"
+var green = onclick="document.getElementById('body').style.backgroundColor='darkgreen'"
+var white = onclick="document.getElementById('body').style.backgroundColor='white'"
+
+function myFunction() {
+    document.getElementById("myDIV").style.animation = "mynewmove 10s 2";
+  }
